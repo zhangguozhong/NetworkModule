b66c12757020e284788240696045e89e9b4e03dd
zhangguozhong <494316382@qq.com>
2018-02-02 22:13:45 +0800

    编辑podspec文件

---
+++ NetworkModule/NetworkModule/Network/NetworkModuleManager.h
@@ -0,0 +1,15 @@
+//
+//  NetworkModuleManager.h
+//  NetworkModule
+//
+//  Created by 张国忠 on 2018/2/2.
+//  Copyright © 2018年 张国忠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkModuleManager : NSObject
+
++(NetworkModuleManager *)apiClient;
+
+@end
